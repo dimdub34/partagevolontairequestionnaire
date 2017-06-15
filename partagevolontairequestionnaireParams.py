f5fe5f3dd9cea25c3a007cf3c07cdc4703f7c0f5
Dimitri DUBOIS <dimitri.dubois@umontpellier.fr>
2017-06-15 13:43:06 +0200

    ajout des codages des échelles

--- partagevolontairequestionnaireParams.py
+++ partagevolontairequestionnaireParams.py
@@ -12,6 +12,23 @@ which means that you should ask to the developer ;-)
 BASELINE = 0
 TREATMENTS_NAMES = {BASELINE: "Baseline"}
 
+NON = PAS_DU_TOUT_D_ACCORD = TROP_ELEVE = NE_SAIT_PAS = 0
+OUI = PAS_D_ACCORD = UN_PEU_ELEVE = ENVIRON = 1
+NI_D_ACCORD_NI_PAS_D_ACCORD = OPTIMAL = 2
+D_ACCORD = UN_PEU_FAIBLE = 3
+TOUT_A_FAIT_D_ACCORD = TROP_FAIBLE = 4
+
+ECHELLE_ELEVE = {TROP_ELEVE: u"Trop élevé", UN_PEU_ELEVE: u"Un peu élevé",
+                 OPTIMAL: u"Optimal", UN_PEU_FAIBLE: u"Un peu faible",
+                 TROP_FAIBLE: u"Trop faible"}
+ECHELLE_ACCORD = {PAS_DU_TOUT_D_ACCORD: u"Pas du tout d'accord",
+                  PAS_D_ACCORD: u"Pas d'accord",
+                  NI_D_ACCORD_NI_PAS_D_ACCORD: u"Ni d'accord ni pas d'accord",
+                  D_ACCORD: u"D'accord",
+                  TOUT_A_FAIT_D_ACCORD: u"Tout à fait d'accord"}
+OUI_NON = {NON: u"Non", OUI: u"Oui"}
+NE_SAIT_PAS_ENVIRON = {NE_SAIT_PAS: u"Je ne sais pas", ENVIRON: u"Environ"}
+
 # parameters -------------------------------------------------------------------
 TREATMENT = BASELINE
 TAUX_CONVERSION = 1
